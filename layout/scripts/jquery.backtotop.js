93dceb8ba15f651d33b618a18f265e269f4bc360
mananjainiitr <76247872+mananjainiitr@users.noreply.github.com>
2021-01-21 20:25:16 +0530

    final submit
    portfolio

---
+++ layout/scripts/jquery.backtotop.js
@@ -0,0 +1,25 @@
+$(".about").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".aboutdiv").offset().top},
+        'slow');
+});
+$(".skill").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".skilldiv").offset().top},
+        'slow');
+});
+$(".academic").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".academicdiv").offset().top},
+        'slow');
+});
+$(".activity").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".activitydiv").offset().top},
+        'slow');
+});
+$(".contact").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".contactdiv").offset().top},
+        'slow');
+});
